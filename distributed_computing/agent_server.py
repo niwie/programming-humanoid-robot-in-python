e51456553aa6b8f4165e329b16b1b38aa672f2b9
n1c3 <=n.wiedersich@campus.tu-berlin.de>
2020-01-16 19:18:14 +0100

    2nd version still with standard lib

--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -14,6 +14,8 @@
 # add PYTHONPATH
 import os
 import sys
+import xmlrpclib
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
 from inverse_kinematics import InverseKinematicsAgent
@@ -37,12 +39,14 @@ class ServerAgent(InverseKinematicsAgent):
         # YOUR CODE HERE
 
     def get_posture(self):
-        return self.posture
+        return self.recognize_posture(self,self.perception)
+        
         '''return current posture of robot'''
         # YOUR CODE HERE
 
     def execute_keyframes(self, keyframes):
-        return self.
+        self.keyframes=keyframes
+        return 1
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
@@ -51,25 +55,29 @@ class ServerAgent(InverseKinematicsAgent):
     def get_transform(self, name):
         '''get transform with given name
         '''
+        return self.transforms[name]
         # YOUR CODE HERE
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
+
+
         # YOUR CODE HERE
-    def init_server():
-        #register at dns
-        server = SimpleXMLRPCServer(("localhost", 8000))
-        print "Listening on port 8000..."
-        server.register_function(get_angle, "get_angle")
-        server.register_function(set_angle, "set_angle")
-        server.register_function(get_posture, "get_posture")
-        server.register_function(execute_keyframes, "execute_keyframes")
-        server.register_function(get_transform, "get_transform")
-        server.register_function(set_transform, "set_transform")
-        server.serve_forever()
+
+
+
 if __name__ == '__main__':
+        #register at dns
+    server = SimpleXMLRPCServer(("localhost", 8000))
+    print "Listening on port 8000..."
+    server.register_function(self.get_angle, "get_angle")
+    server.register_function(self.set_angle, "set_angle")
+    server.register_function(self.get_posture, "get_posture")
+    server.register_function(self.execute_keyframes, "execute_keyframes")
+    server.register_function(self.get_transform, "get_transform")
+    server.register_function(self.set_transform, "set_transform")
+    server.serve_forever()
     agent = ServerAgent()
-    agent.init_server()
     agent.run()
 
