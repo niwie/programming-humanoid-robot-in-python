2d0dacadf2f8b1777550fa621ef3eea0def3aeaf
n1c3 <=n.wiedersich@campus.tu-berlin.de>
2020-01-16 15:42:48 +0100

    added distributed xml rpc, but will prob. change it by implementing rpc with pyro4 because it is much easier

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -9,7 +9,7 @@
 import weakref
 
 class PostHandler(object):
-    '''the post hander wraps function to be excuted in paralle
+    '''the post hander wraps function to be excuted in parallel
     '''
     def __init__(self, obj):
         self.proxy = weakref.proxy(obj)
@@ -33,29 +33,69 @@ class ClientAgent(object):
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        try:
+            self.post.proxy.get_angle(joint_name)
+        except xmlrpclib.Fault as err:
+        print "A fault occurred"
+        print "Fault code: %d" % err.faultCode
+        print "Fault string: %s" % err.faultString
     
     def set_angle(self, joint_name, angle):
+        try:
+            self.post.proxy.set_angle(joint_name)
+        except xmlrpclib.Fault as err:
+        print "A fault occurred"
+        print "Fault code: %d" % err.faultCode
+        print "Fault string: %s" % err.faultString
+    
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
 
     def get_posture(self):
-        '''return current posture of robot'''
+        #return value!
+        try:
+            self.post.proxy.get_posture()
+        except xmlrpclib.Fault as err:
+        print "A fault occurred"
+        print "Fault code: %d" % err.faultCode
+        print "Fault string: %s" % err.faultString
+    '''return current posture of robot'''
         # YOUR CODE HERE
 
     def execute_keyframes(self, keyframes):
+        try:
+            self.post.proxy.execute_keyframes(keyframes)
+        except xmlrpclib.Fault as err:
+        print "A fault occurred"
+        print "Fault code: %d" % err.faultCode
+        print "Fault string: %s" % err.faultString
+    
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
 
     def get_transform(self, name):
-        '''get transform with given name
+        try:
+            self.post.proxy.get_transform(name)
+        except xmlrpclib.Fault as err:
+        print "A fault occurred"
+        print "Fault code: %d" % err.faultCode
+        print "Fault string: %s" % err.faultString
+    '''get transform with given name
         '''
         # YOUR CODE HERE
 
     def set_transform(self, effector_name, transform):
-        '''solve the inverse kinematics and control joints use the results
+        #return
+        try:
+            self.post.proxy.set_transform(effector_name,transform)
+        except xmlrpclib.Fault as err:
+        print "A fault occurred"
+        print "Fault code: %d" % err.faultCode
+        print "Fault string: %s" % err.faultString
+    '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
 
--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -26,18 +26,23 @@ class ServerAgent(InverseKinematicsAgent):
     
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
+        self.perception.join[joint_name]
         # YOUR CODE HERE
     
     def set_angle(self, joint_name, angle):
+        self.target_joints[joint_name] = angle
+        return 1
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
 
     def get_posture(self):
+        return self.posture
         '''return current posture of robot'''
         # YOUR CODE HERE
 
     def execute_keyframes(self, keyframes):
+        return self.
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
@@ -52,8 +57,19 @@ class ServerAgent(InverseKinematicsAgent):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
-
+    def init_server():
+        #register at dns
+        server = SimpleXMLRPCServer(("localhost", 8000))
+        print "Listening on port 8000..."
+        server.register_function(get_angle, "get_angle")
+        server.register_function(set_angle, "set_angle")
+        server.register_function(get_posture, "get_posture")
+        server.register_function(execute_keyframes, "execute_keyframes")
+        server.register_function(get_transform, "get_transform")
+        server.register_function(set_transform, "set_transform")
+        server.serve_forever()
 if __name__ == '__main__':
     agent = ServerAgent()
+    agent.init_server()
     agent.run()
 
